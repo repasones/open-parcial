1de9b53e14cca49565514817007c1af0779e613b
Franco Durand <francoduve.25@gmail.com>
2024-10-03 00:58:50 -0500

    Refactorizar componente de tarjeta para mostrar el número de asistentes registrados y el porcentaje de asistencia

--- src/app/registration/components/card/card.component.ts
+++ src/app/registration/components/card/card.component.ts
@@ -1,6 +1,8 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input, OnInit } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { Event } from '../../model/event';
+import { RegistrationApiService } from '../../services/registration-api.service';
+import { Attendee } from '../../model/attendee';
 
 @Component({
   selector: 'app-card',
@@ -9,7 +11,20 @@ import { Event } from '../../model/event';
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
-export class CardComponent {
+export class CardComponent implements OnInit {
   @Input() event!: Event;
+  api = inject(RegistrationApiService);
+  registeredAttendees = 0;
+  attendance = 0;
 
+  ngOnInit(): void {
+    this.api.getAttendeesByEventId(this.event.id).subscribe((data: any) => {
+      this.registeredAttendees = data.length;
+
+      const asistentes = data.filter((a: Attendee) => a.checkedInAt != null);
+
+      this.attendance = asistentes.length / this.registeredAttendees * 100;
+      this.attendance = Number(this.attendance.toFixed(2));
+    })
+  }
 }

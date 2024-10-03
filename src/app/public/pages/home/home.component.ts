cf393aa57cf44e3b2ef9055cc0a2bc02a362f064
Franco Durand <francoduve.25@gmail.com>
2024-10-03 00:30:53 -0500

    cards en home

--- src/app/public/pages/home/home.component.ts
+++ src/app/public/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CardComponent } from "../../../registration/components/card/card.component";
+import { ListComponent } from "../../../registration/components/list/list.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CardComponent],
+  imports: [CardComponent, ListComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })

5107aed56f9a94191b39b43c14a71533464c6e8f
Franco Durand <francoduve.25@gmail.com>
2024-10-03 01:44:59 -0500

    Agregar traducción al componente de inicio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./public/pages/home/home.component";
 import { ToolbarComponent } from "./public/components/toolbar/toolbar.component";
+import { TranslateService } from "@ngx-translate/core";
 
 @Component({
   selector: 'app-root',
@@ -12,4 +13,9 @@ import { ToolbarComponent } from "./public/components/toolbar/toolbar.component"
 })
 export class AppComponent {
   title = 'parcialopen';
+
+  constructor(private translateService: TranslateService) {
+    this.translateService.use('en');
+    this.translateService.setDefaultLang('en');
+  }
 }
--- src/app/public/pages/home/home.component.ts
+++ src/app/public/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CardComponent } from "../../../registration/components/card/card.component";
 import { ListComponent } from "../../../registration/components/list/list.component";
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CardComponent, ListComponent],
+  imports: [CardComponent, ListComponent, TranslateModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })

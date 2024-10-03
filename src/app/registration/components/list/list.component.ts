75139da75cb3a619bf17d5eb276669eb952b6d75
Franco Durand <francoduve.25@gmail.com>
2024-10-03 00:42:28 -0500

    Refactorizar componente de lista para mostrar eventos dinámicamente

--- src/app/registration/components/list/list.component.ts
+++ src/app/registration/components/list/list.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CardComponent } from "../card/card.component";
-
+import { Event } from '../../model/event';
+import { RegistrationApiService } from '../../services/registration-api.service';
+import { OnInit } from '@angular/core';
 @Component({
   selector: 'app-list',
   standalone: true,
@@ -8,6 +10,16 @@ import { CardComponent } from "../card/card.component";
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
-export class ListComponent {
+export class ListComponent implements OnInit {
+  events: Array<Event> = [];
+  api = inject(RegistrationApiService);
 
+
+  ngOnInit(): void {
+    this.api.getAllEvents().subscribe((data: any) => {
+      //console.log(data);
+      this.events = data;
+      console.log(this.events)
+    })
+  }
 }
